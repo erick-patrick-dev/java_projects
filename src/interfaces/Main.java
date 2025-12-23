e2b10cf702f72d0d7a9fdaf732b318d51c179069
Erick Patrick <94844939+erick-patrick-dev@users.noreply.github.com>
2025-12-23 19:48:51 -0300

    Add files via upload

---
+++ src/interfaces/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        var musicPlayer = new MusicPlayer() {
+//            @Override
+//            public void playMusic() {
+//                System.out.println("Tocando a música");
+//            }
+//
+//            @Override
+//            public void pauseMusic() {
+//
+//            }
+//
+//            @Override
+//            public void stopMusic() {
+//
+//            }
+//        };
+
+//        musicPlayer.playMusic();
+//        System.out.println(musicPlayer.getClass());
+    }
+}
