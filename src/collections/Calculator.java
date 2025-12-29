cae637016bdb12282cc85464bb9b010285ac3efb
Erick Patrick <94844939+erick-patrick-dev@users.noreply.github.com>
2025-12-29 14:13:04 -0300

    Trabalhando com Enum

---
+++ src/collections/Calculator.java
@@ -0,0 +1,39 @@
+import enumerate.OperationEnum;
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public void run(){
+        var scanner = new Scanner(System.in);
+        var option = -1;
+
+        while (option != 5){
+            System.out.println("Escolha uma opção pelo número");
+            System.out.println("1 - Soma");
+            System.out.println("2 - Subtração");
+            System.out.println("3 - Multiplicação");
+            System.out.println("4 - Divisão");
+            System.out.println("5 - Sair");
+            option = scanner.nextInt();
+
+            if (option > 5 || option < 1){
+                System.out.println("Escolha uma opção válida");
+                continue;
+            }
+            if (option == 5) break;
+
+            var selectedOption = OperationEnum.values()[option - 1];
+            //var selectedOption = OperationEnum.valueOf("SUM"); Outra opção de consulta
+
+            System.out.println("Informe o primeiro valor");
+            var value1 = scanner.nextInt();
+            System.out.println("Informe o segundo valor");
+            var value2 = scanner.nextInt();
+
+            var result = selectedOption.getCalculate().apply(value1, value2);
+
+            System.out.printf("%s %s %s = %s \n\n", value1, selectedOption.getSymbol(), value2, result);
+        }
+    }
+}
