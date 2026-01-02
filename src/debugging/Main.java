2f91876a057be304af0fdb4f3f23173a9b5728b1
Erick Patrick <94844939+erick-patrick-dev@users.noreply.github.com>
2026-01-02 17:59:40 -0300

    Tratamento de exceções

---
+++ src/debugging/Main.java
@@ -0,0 +1,144 @@
+import br.com.dio.dao.UserDAO;
+import br.com.dio.exception.CustomException;
+import br.com.dio.exception.EmptyStorageException;
+import br.com.dio.exception.UserNotFoundException;
+import br.com.dio.exception.ValidatorException;
+import br.com.dio.model.MenuOption;
+import br.com.dio.model.UserModel;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import static br.com.dio.validator.UserValidator.verifyModel;
+
+public class Main {
+
+    private final static UserDAO dao = new UserDAO();
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        while (true){
+            System.out.println("\nBem-vindo ao cadastro de usuários. Selecione a operação desejada:");
+            System.out.println("1 - Cadastrar");
+            System.out.println("2 - Atualizar");
+            System.out.println("3 - Excluir");
+            System.out.println("4 -Buscar por identificador");
+            System.out.println("5 - Listar");
+            System.out.println("6 - Sair");
+            var userInput = scanner.nextInt();
+            var selectedOption = MenuOption.values()[userInput -1];
+            switch (selectedOption){
+                case SAVE -> {
+                    try {
+                        var user = dao.save(requestToSave());
+                        System.out.printf("Usuário cadastrado: %s", user);
+                    }catch (CustomException ex){
+                        System.out.println(ex.getMessage());
+                        ex.printStackTrace();
+                    }
+                }
+                case UPDATE -> {
+                    try {
+                        var user = dao.update(requestToUpdate());
+                        System.out.printf("Usuário atualizado: %s", user);
+                    } catch (UserNotFoundException | EmptyStorageException ex){
+                        System.out.println(ex.getMessage());
+                    } catch(CustomException vex) {
+                        System.out.println(vex.getMessage());
+                        vex.printStackTrace();
+                    }finally {
+                        System.out.println("====================");
+                    }
+                }
+                case DELETE -> {
+                    try{
+                        dao.delete(requestId());
+                        System.out.println("Usuário excluído");
+                    } catch (UserNotFoundException | EmptyStorageException ex){
+                        System.out.println(ex.getMessage());
+                    } finally {
+                        System.out.println("====================");
+                    }
+                }
+                case FIND_BY_ID -> {
+                    try {
+                        var id = requestId();
+                        var user = dao.findById(id);
+                        System.out.printf("Usuário com id %s:", id);
+                        System.out.println(user);
+                    } catch (UserNotFoundException | EmptyStorageException ex){
+                        System.out.println(ex.getMessage());
+                    }
+                }
+                case FIND_ALL -> {
+                    var users = dao.findAll();
+                    System.out.println("====================");
+                    System.out.println("Usuários cadastrados");
+                    users.forEach(System.out::println);
+                    System.out.println("==========fim==========");
+                }
+                case EXIT -> System.exit(0);
+            }
+        }
+    }
+
+    private static long requestId(){
+        System.out.println("Informe o identificador do usuário");
+        return scanner.nextLong();
+    }
+
+    private static String requestName() {
+        System.out.println("Informe o nome do usuário");
+        var res = scanner.next();
+        return res;
+    }
+
+    private static String requestEmail() {
+        System.out.println("Informe o e-mail do usuário");
+        var email = scanner.next();
+        return email;
+    }
+
+    private static LocalDate requestBirthday() {
+        System.out.println("Informe o data de nascimento do usuário (dd/MM/yyyy)");
+        var birthdayString = scanner.next();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        var birthday = LocalDate.parse(birthdayString, formatter);
+        return birthday;
+    }
+
+    private static UserModel requestToSave() {
+        var name = requestName();
+        var email = requestEmail();
+        var birthday = requestBirthday();
+
+        return validateInputs(0, name, email, birthday);
+    }
+
+    private static UserModel validateInputs(final long id, final String name, final String email, final LocalDate birthday) {
+
+        var user = new UserModel(0, name, email, birthday);
+        try {
+            verifyModel(user);
+            return user;
+        }catch (ValidatorException ex){
+            throw new CustomException("O seu usuário contém erros" + ex.getMessage(), ex);
+        }
+    }
+
+    private static UserModel requestToUpdate() {
+        var id = requestId();
+        var name = requestName();
+        var email = requestEmail();
+        var birthday = requestBirthday();
+
+        return validateInputs(id, name, email, birthday);
+    }
+
+
+
+
+}
